27fd14055f53f2096f26150634c3103ebb6f38ce
qdecross <quentin.decrossac@gmail.com>
2026-07-03 14:17:08 +0200

    Implement collectible setup and scoring logic in GameView and enhance Player class with life and score management

--- src/arcade_views/game_view.py
+++ src/arcade_views/game_view.py
@@ -24,6 +24,55 @@ class GameView(arcade.View):
         start_col = self.cols // 2
         self.player = Player(start_row, start_col)
 
+        self.pacgums: set[tuple[int, int]] = set()
+        self.super_pacgums: set[tuple[int, int]] = set()
+
+        self.setup_collectibles()
+
+    def setup_collectibles(self) -> None:
+        """
+        Populate the maze with pacgums and place super-pacgums
+        in the 4 corners.
+        """
+        corners = [
+            (0, 0),
+            (0, self.cols - 1),
+            (self.rows - 1, 0),
+            (self.rows - 1, self.cols - 1)
+        ]
+
+        for r in range(self.rows):
+            for c in range(self.cols):
+                if (r, c) in corners:
+                    self.super_pacgums.add((r, c))
+                elif (r, c) != (self.player.row, self.player.col):
+                    self.pacgums.add((r, c))
+
+    def on_update(self, delta_time: float) -> None:
+        """
+        Update game logic: handle collectible consumption and game rules.
+
+        Args:
+            delta_time (float): Time elapsed since the last frame.
+        """
+        current_pos = (self.player.row, self.player.col)
+
+        if current_pos in self.pacgums:
+            self.pacgums.remove(current_pos)
+            self.player.add_score(10)  # Value can later be set from conf.json
+
+        if current_pos in self.super_pacgums:
+            self.super_pacgums.remove(current_pos)
+            self.player.add_score(50)  # Value can later be set from conf.json
+            # TODO: Make ghosts edible here
+
+        # Placeholder for Ghost Collision
+        # if self.check_ghost_collision():
+        #     is_dead = self.player.lose_life()
+        #     if is_dead:
+        #         print("Game Over!")
+        #         # Trigger Game Over View
+
     def on_draw(self) -> None:
         """
         Render the maze walls and the player object.
@@ -56,6 +105,16 @@ class GameView(arcade.View):
                 if cell_value & WEST:
                     arcade.draw_line(x_left, y_top, x_left, y_bottom,
                                      arcade.color.BLACK, 2)
+
+        for r, c in self.pacgums:
+            x = start_x_offset + (c * CELL_SIZE) + (CELL_SIZE / 2)
+            y = start_y_offset - (r * CELL_SIZE) - (CELL_SIZE / 2)
+            arcade.draw_circle_filled(x, y, CELL_SIZE / 8, arcade.color.BLUE)
+
+        for r, c in self.super_pacgums:
+            x = start_x_offset + (c * CELL_SIZE) + (CELL_SIZE / 2)
+            y = start_y_offset - (r * CELL_SIZE) - (CELL_SIZE / 2)
+            arcade.draw_circle_filled(x, y, CELL_SIZE / 4, arcade.color.RED)
 
         self.player.draw(start_x_offset, start_y_offset, CELL_SIZE)
 
