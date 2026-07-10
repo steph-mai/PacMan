40ef626ad9d10c27664f9ec7e608d8512646a126
Stephanie Maire <stmaire@z1r10p2.42lyon.fr>
2026-07-10 14:53:01 +0200

    add Blinky(red ghost behavior)

--- src/ai/personalities.py
+++ src/ai/personalities.py
@@ -35,13 +35,66 @@ def _get_reverse_direction(direction: int) -> int:
     if direction == WEST:
         return EAST
 
+
+def _get_valid_directions(
+        ghost: "Ghost",
+        maze: list[list[int]]) -> list[int]:
+
+    current_walls = maze[ghost.row][ghost.col]
+
+    valid_directions = []
+    if not (current_walls & NORTH):
+        valid_directions.append(NORTH)
+    if not (current_walls & WEST):
+        valid_directions.append(WEST)
+    if not (current_walls & SOUTH):
+        valid_directions.append(SOUTH)
+    if not (current_walls & EAST):
+        valid_directions.append(EAST)
+    reverse_direction = _get_reverse_direction(ghost.current_direction)
+    if reverse_direction in valid_directions and len(valid_directions) > 1:
+        valid_directions.remove(reverse_direction)
+
+    return valid_directions
+
+
 def shadow_personality_move(
-        ghost: Any,
+        ghost: "Ghost",
         maze: list[list[int]],
         player_row: int,
         player_col: int
         ) -> int:
-    pass
+
+    valid_directions = _get_valid_directions(ghost, maze)
+    if not valid_directions:
+        return 0
+
+    best_direction = 0
+    min_distance = float('inf')
+
+    for direction in valid_directions:
+        next_row = ghost.row
+        next_col = ghost.col
+        if direction == NORTH:
+            next_row -= 1
+        elif direction == EAST:
+            next_col += 1
+        elif direction == SOUTH:
+            next_row += 1
+        elif direction == WEST:
+            next_col -= 1
+
+        distance = calculate_as_the_crow_flies_distance(next_row, next_col, player_row, player_col)
+
+        if distance < min_distance:
+            min_distance = distance
+            best_direction = direction
+
+    return best_direction
+
+
+def calculate_as_the_crow_flies_distance(row1, col1, row2, col2):
+    return (row1 - row2)**2 + (col1 - col2)**2
 
 
 def speedy_personality_move(
@@ -78,21 +131,7 @@ def random_move(
         player_col: int
         ) -> int:
 
-    current_walls = maze[ghost.row][ghost.col]
-
-    valid_directions = []
-    if not (current_walls & NORTH):
-        valid_directions.append(NORTH)
-    if not (current_walls & EAST):
-        valid_directions.append(EAST)
-    if not (current_walls & SOUTH):
-        valid_directions.append(SOUTH)
-    if not (current_walls & WEST):
-        valid_directions.append(WEST)
-    reverse_direction = _get_reverse_direction(ghost.current_direction)
-    if reverse_direction in valid_directions and len(valid_directions) > 1:
-        valid_directions.remove(reverse_direction)
-
+    valid_directions = _get_valid_directions(ghost, maze)
     return random.choice(valid_directions)
 
 
--- src/arcade_views/game_view.py
+++ src/arcade_views/game_view.py
@@ -120,7 +120,7 @@ class GameView(arcade.View):
             # (self.rows - 2, 0, arcade.color.PINK, GhostPersonality.SPEEDY),
             # (self.rows - 2, self.cols - 1,
             #  arcade.color.ORANGE, GhostPersonality.POKEY)
-            (1, 0, arcade.color.RED, GhostPersonality.RANDOM),
+            (1, 0, arcade.color.RED, GhostPersonality.SHADOW),
             (1, self.cols - 1, arcade.color.CYAN, GhostPersonality.RANDOM),
             (self.rows - 2, 0, arcade.color.PINK, GhostPersonality.RANDOM),
             (self.rows - 2, self.cols - 1,
